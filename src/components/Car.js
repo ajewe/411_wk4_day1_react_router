1a3def3e10c2a627135489ed33b423415ca826ce
ajewe <ajewe@utexas.edu>
2020-04-08 12:30:47 -0500

    added router and made routes and updated each car with specific info from api

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import cars from '../cars.json'
-// import material ui components here //
-// Container, Paper, Chip //
+import { Container, Paper, Chip } from '@material-ui/core'
 
 const Car = (props) => {
-    return (
-        <h1>A specific car</h1>
-    )
+  let id = props.match.params.id
+  let specificCar = cars[id - 1]
+
+  const chipData = [
+    `id: ${specificCar.id}`,
+    `Name: ${specificCar.Name}`,
+    `mpg: ${specificCar.Miles_per_Gallon}`,
+    `Cylinders: ${specificCar.Cylinders}`,
+    `Displacement: ${specificCar.Displacement}`,
+    `Horsepower: ${specificCar.Horsepower}`,
+    `Weight(lbs): ${specificCar.Weight_in_lbs}`,
+    `Acceleration: ${specificCar.Acceleration}`,
+    `Year: ${specificCar.Year}`,
+    `Origin: ${specificCar.Origin}`,
+  ]
+
+  return (
+    <Container maxWidth="sm" style={{marginTop: "40px"}}>
+      <Paper elevation={3} style={{padding: "40px"}}>
+        <h2>{specificCar.Name}</h2>
+        {chipData.map((data, i) => <Chip
+          key={i}
+          label={data}
+        />
+        )
+        }
+      </Paper>
+    </Container>
+  )
 }
 
 export default Car
